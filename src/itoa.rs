31bb71d21bb891c3e5e06932564a182646c3298e
Roma Sokolov <sokolov.r.v@gmail.com>
2018-04-27 19:23:36 +0200

    [add] failed attempt to get gyro data

---
+++ src/itoa.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+macro_rules! itoa_impl {
+    ($num:expr, $num_digits:expr, $buffer:ident) => (
+        let mut x = $num;
+        $buffer[$num_digits - 1] = '0' as u8;
+        let mut i = $num_digits - 1;
+        while x > 0 {
+            let digit = (x % 10) as u8;
+            x = x / 10;
+            $buffer[i] = ('0' as u8 + digit) as u8;
+            i -= 1;
+        }
+    );
+}
+
+pub fn itoa_i16(num: i16) -> [u8; 6] {
+    const NUM_DIGITS: usize = 6;
+    let mut buffer: [u8; NUM_DIGITS] = [' ' as u8; NUM_DIGITS];
+    itoa_impl!(num, NUM_DIGITS, buffer);
+    return buffer;
+}
--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,59 @@
 
 extern crate cortex_m;
 extern crate cortex_m_rtfm as rtfm;
-extern crate nb;
 extern crate panic_abort;
 extern crate embedded_hal as ehal;
 extern crate stm32f30x_hal as hal;
 
-// extern crate mpu9250;
+extern crate mpu9250;
+
+extern crate nb;
+
 
 mod beeper;
+mod itoa;
 
 use hal::prelude::*;
 use hal::stm32f30x;
 use hal::delay::Delay;
-// use mpu9250::Mpu9250;
 use hal::serial;
+use hal::spi::Spi;
+use hal::serial::{Serial, Tx, Rx};
+use hal::gpio::gpiob;
+use hal::gpio::{Output, PushPull, AF5};
+
+use mpu9250::Mpu9250;
 use rtfm::{app, Threshold};
 use cortex_m::register::msp;
 
 const BAUD_RATE: hal::time::Bps = hal::time::Bps(9600);
 
+type MPU9250 = mpu9250::Mpu9250<
+        Spi<hal::stm32f30x::SPI1, (gpiob::PB3<AF5>,
+                                   gpiob::PB4<AF5>,
+                                   gpiob::PB5<AF5>)>,
+    gpiob::PB9<Output<PushPull>>,
+    mpu9250::Imu>;
+
 app!{
     device: stm32f30x,
 
     resources: {
-        static TX: hal::serial::Tx<hal::stm32f30x::USART1>;
-        static RX: hal::serial::Rx<hal::stm32f30x::USART1>;
+        static TX: Tx<hal::stm32f30x::USART1>;
+        static RX: Rx<hal::stm32f30x::USART1>;
         static BEEPER: beeper::Beeper;
         static DELAY: hal::delay::Delay;
+        static MPU: MPU9250;
     },
 
     tasks: {
         USART1_EXTI25: {
             path: echo,
-            resources: [TX, RX, BEEPER, DELAY],
+            resources: [TX, RX, BEEPER, DELAY, MPU],
+        },
+        SYS_TICK: {
+            path: tick,
+            resources: [MPU, BEEPER, DELAY, TX],
         },
     }
 }
@@ -45,21 +65,44 @@ app!{
 fn init(p: init::Peripherals) -> init::LateResources {
     let mut rcc = p.device.RCC.constrain();
     let mut gpioa = p.device.GPIOA.split(&mut rcc.ahb);
+    let mut gpiob = p.device.GPIOB.split(&mut rcc.ahb);
     let gpioc = p.device.GPIOC.split(&mut rcc.ahb);
 
     let mut flash = p.device.FLASH.constrain();
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
-    let delay = Delay::new(p.core.SYST, clocks);
+    let mut delay = Delay::new(p.core.SYST, clocks);
     let txpin = gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
     let rxpin = gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
-    let mut serial = hal::serial::Serial::usart1(
+    let mut serial = Serial::usart1(
         p.device.USART1,
         (txpin, rxpin),
         BAUD_RATE, clocks, &mut rcc.apb2);
     serial.listen(serial::Event::Rxne);
     let (tx, rx) = serial.split();
     let beep = beeper::Beeper::new(gpioc);
-    init::LateResources { TX: tx, RX: rx, BEEPER: beep, DELAY: delay }
+
+    // SPI1
+    let nss = gpiob.pb9.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+    let sck = gpiob.pb3.into_af5(&mut gpiob.moder, &mut gpiob.afrl);
+    let miso = gpiob.pb4.into_af5(&mut gpiob.moder, &mut gpiob.afrl);
+    let mosi = gpiob.pb5.into_af5(&mut gpiob.moder, &mut gpiob.afrl);
+
+    let spi = Spi::spi1(
+        p.device.SPI1,
+        (sck, miso, mosi),
+        mpu9250::MODE,
+        1.mhz(),
+        clocks,
+        &mut rcc.apb2,
+    );
+
+    let mpu9250 = Mpu9250::imu(spi, nss, &mut delay).unwrap();
+
+    init::LateResources { TX: tx,
+                          RX: rx,
+                          BEEPER: beep,
+                          DELAY: delay,
+                          MPU: mpu9250 }
 }
 
 // IDLE LOOP
@@ -75,6 +118,26 @@ fn system_reset() {
     unsafe {
         (*scb).aircr.write(0x05FA0000 | 0x04u32);
     }
+}
+
+fn tick(_t: &mut Threshold, mut r: SYS_TICK::Resources) {
+    let rb = &mut r.BEEPER;
+    rb.on();
+    let rd = &mut r.DELAY;
+    let mut tx = r.TX;
+    let gyrox = match r.MPU.gx() {
+        Ok(x) => {
+            x
+        }
+        Err(_) => {
+            0
+        }
+    };
+    let bb = itoa::itoa_i16(gyrox);
+    for b in bb.iter() {
+        wrt(&mut tx, b.clone(), rb, rd, 2000);
+    }
+    rb.off();
 }
 
 // TASKS
