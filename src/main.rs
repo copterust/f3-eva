2adf92b15d2e72d8add2ec39d2911ff3082f3123
Roma Sokolov <sokolov.r.v@gmail.com>
2018-05-25 20:29:17 +0200

    Get rid of cortex-m-rtfm; first steps

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 //#![deny(unsafe_code)]
-#![deny(warnings)]
+// #![deny(warnings)]
 #![no_std]
+#![no_main]
 #![feature(proc_macro)]
 
 extern crate cortex_m;
-extern crate cortex_m_rtfm as rtfm;
-extern crate panic_abort;
-
+#[macro_use]
+extern crate cortex_m_rt as rt;
 extern crate nb;
+extern crate panic_abort;
 
 extern crate embedded_hal as ehal;
 extern crate mpu9250;
@@ -19,12 +20,10 @@ mod debug_writer;
 mod itoa;
 mod motors;
 
-use motors::Motor;
-
 use bootloader::Bootloader;
 use debug_writer::DebugWrite;
+use motors::Motor;
 
-use hal::delay::Delay;
 use hal::gpio::gpiob;
 use hal::gpio::{AF5, Output, PushPull};
 use hal::prelude::*;
@@ -35,8 +34,9 @@ use hal::stm32f30x;
 use hal::time::Hertz;
 use hal::timer::{self, Timer};
 
-use mpu9250::Mpu9250;
-use rtfm::{app, Threshold};
+use cortex_m::asm;
+use rt::ExceptionFrame;
+// use mpu9250::Mpu9250;
 
 const BAUD_RATE: hal::time::Bps = hal::time::Bps(9600);
 const FREQ: u32 = 1024;
@@ -53,41 +53,46 @@ type DW = debug_writer::DebugWriter<
     hal::timer::Timer<cortex_m::peripheral::SYST>,
 >;
 
-app!{
-    device: stm32f30x,
+// app!{
+//     device: stm32f30x,
 
-    resources: {
-        static DW: DW;
-        static RX: Rx<hal::stm32f30x::USART1>;
-        // can't use bootloader trait here as the size of the resource
-        // should be known at compile time
-        static BOOTLOADER: bootloader::stm32f30x::Bootloader;
-        static MPU: MPU9250;
-        static MOTORS: motors::f3evo::MotorPWM;
-    },
+//     resources: {
+//         static DW: DW;
+//         static RX: Rx<hal::stm32f30x::USART1>;
+//         // can't use bootloader trait here as the size of the resource
+//         // should be known at compile time
+//         static BOOTLOADER: bootloader::stm32f30x::Bootloader;
+//         static MPU: MPU9250;
+//         static MOTORS: motors::f3evo::MotorPWM;
+//     },
 
-    tasks: {
-        USART1_EXTI25: {
-            path: echo,
-            resources: [DW, RX, BOOTLOADER, MOTORS],
-        },
-        SYS_TICK: {
-            path: tick,
-            resources: [MPU, DW],
-        },
-    }
-}
+//     tasks: {
+//         USART1_EXTI25: {
+//             path: echo,
+//             resources: [DW, RX, BOOTLOADER, MOTORS],
+//         },
+//         SYS_TICK: {
+//             path: tick,
+//             resources: [MPU, DW],
+//         },
+//     }
+// }
 
-fn init(p: init::Peripherals) -> init::LateResources {
-    let bootloader = bootloader::stm32f30x::Bootloader::new(p.core.SCB);
+entry!(main);
 
-    let mut rcc = p.device.RCC.constrain();
-    let mut gpioa = p.device.GPIOA.split(&mut rcc.ahb);
-    let mut gpiob = p.device.GPIOB.split(&mut rcc.ahb);
-    let gpioc = p.device.GPIOC.split(&mut rcc.ahb);
+fn main() -> ! {
+    let device = stm32f30x::Peripherals::take().unwrap();
+    let core = cortex_m::Peripherals::take().unwrap();
+    let bootloader = bootloader::stm32f30x::Bootloader::new();
 
-    let mut flash = p.device.FLASH.constrain();
-    let clocks = rcc.cfgr
+    let mut rcc = device.RCC.constrain();
+    let mut gpioa = device.GPIOA.split(&mut rcc.ahb);
+    let mut gpiob = device.GPIOB.split(&mut rcc.ahb);
+    let gpioc = device.GPIOC.split(&mut rcc.ahb);
+
+    let mut flash = device.FLASH.constrain();
+    let clocks = rcc
+        .cfgr
         .sysclk(64.mhz())
         .pclk1(32.mhz())
         .freeze(&mut flash.acr);
@@ -95,7 +100,7 @@ fn init(p: init::Peripherals) -> init::LateResources {
     let txpin = gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
     let rxpin = gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
     let mut serial = Serial::usart1(
-        p.device.USART1,
+        device.USART1,
         (txpin, rxpin),
         BAUD_RATE,
         clocks,
@@ -115,7 +120,7 @@ fn init(p: init::Peripherals) -> init::LateResources {
     let mosi = gpiob.pb5.into_af5(&mut gpiob.moder, &mut gpiob.afrl);
 
     let spi = Spi::spi1(
-        p.device.SPI1,
+        device.SPI1,
         (sck, miso, mosi),
         mpu9250::MODE,
         1.mhz(),
@@ -123,78 +128,80 @@ fn init(p: init::Peripherals) -> init::LateResources {
         &mut rcc.apb2,
     );
 
-    let mut delay = Delay::new(p.core.SYST, clocks);
-    let mpu9250 = Mpu9250::imu(spi, nss, &mut delay).unwrap();
-    let mut timer = Timer::syst(delay.free(), FREQ.hz(), clocks);
+    // let mpu9250 = Mpu9250::imu(spi, nss, &mut delay).unwrap();
+
+    let tim2 = device.TIM2;
+    let mut timer = Timer::tim2(tim2, FREQ.hz(), clocks, &mut rcc.apb1);
     timer.listen(timer::Event::TimeOut);
     let beep = beeper::Beeper::new(gpioc);
 
     let mut dw = debug_writer::DebugWriter::new(tx, timer, beep, BEEP_TIMEOUT);
     dw.debug('i');
     let motor = motors::f3evo::MotorPWM::new();
-    init::LateResources {
-        DW: dw,
-        RX: rx,
-        BOOTLOADER: bootloader,
-        MPU: mpu9250,
-        MOTORS: motor,
-    }
-}
 
-// IDLE LOOP
-fn idle() -> ! {
-    // Sleep
     loop {
-        rtfm::wfi();
+        asm::bkpt();
     }
 }
 
-fn tick(_t: &mut Threshold, _r: SYS_TICK::Resources) {
-    // let dw = &mut r.DW;
-    // let gyrox = match r.MPU.gx() {
-    //     Ok(x) => x,
-    //     Err(_) => 0,
-    // };
-    // let data = itoa::itoa_i16(gyrox);
-    // dw.debug(&data[..]);
+exception!(HardFault, hard_fault);
+
+fn hard_fault(ef: &ExceptionFrame) -> ! {
+    panic!("HardFault at {:#?}", ef);
 }
 
-// TASKS
-// Send back the received byte
-fn echo(_t: &mut Threshold, r: USART1_EXTI25::Resources) {
-    let mut rx = r.RX;
-    let mut dw = r.DW;
-    let mut bootloader = r.BOOTLOADER;
-    let mut motor = r.MOTORS;
-    match rx.read() {
-        Ok(b) => {
-            if b == 'r' as u8 {
-                motor.write();
-                bootloader.system_reset();
-            }
+exception!(*, default_handler);
 
-            if b == 'R' as u8 {
-                bootloader.to_bootloader();
-            }
-            dw.debug(b);
-        }
-        Err(nb::Error::Other(e)) => match e {
-            serial::Error::Framing => {
-                dw.error('f');
-            }
-            serial::Error::Overrun => {
-                rx.clear_overrun_error();
-            }
-            serial::Error::Parity => {
-                dw.error('p');
-            }
-            serial::Error::Noise => {
-                dw.error('n');
-            }
-            _ => {
-                dw.error('u');
-            }
-        },
-        Err(nb::Error::WouldBlock) => {}
-    };
+fn default_handler(irqn: i16) {
+    panic!("Unhandled exception (IRQn = {})", irqn);
 }
+
+// fn tick(_t: &mut Threshold, _r: SYS_TICK::Resources) {
+//     // let dw = &mut r.DW;
+//     // let gyrox = match r.MPU.gx() {
+//     //     Ok(x) => x,
+//     //     Err(_) => 0,
+//     // };
+//     // let data = itoa::itoa_i16(gyrox);
+//     // dw.debug(&data[..]);
+// }
+
+// // TASKS
+// // Send back the received byte
+// fn echo(_t: &mut Threshold, r: USART1_EXTI25::Resources) {
+//     let mut rx = r.RX;
+//     let mut dw = r.DW;
+//     let mut bootloader = r.BOOTLOADER;
+//     let mut motor = r.MOTORS;
+//     match rx.read() {
+//         Ok(b) => {
+//             if b == 'r' as u8 {
+//                 motor.write();
+//                 bootloader.system_reset();
+//             }
+
+//             if b == 'R' as u8 {
+//                 bootloader.to_bootloader();
+//             }
+//             dw.debug(b);
+//         }
+//         Err(nb::Error::Other(e)) => match e {
+//             serial::Error::Framing => {
+//                 dw.error('f');
+//             }
+//             serial::Error::Overrun => {
+//                 rx.clear_overrun_error();
+//             }
+//             serial::Error::Parity => {
+//                 dw.error('p');
+//             }
+//             serial::Error::Noise => {
+//                 dw.error('n');
+//             }
+//             _ => {
+//                 dw.error('u');
+//             }
+//         },
+//         Err(nb::Error::WouldBlock) => {}
+//     };
+// }
