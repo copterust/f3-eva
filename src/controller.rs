d6cf2f129684ecc497995ae07a8fcddecbc27941
Alexander Zhuravlev <123368+bofh@users.noreply.github.com>
2018-09-10 20:05:44 +0200

    Add altitude controller

--- src/controller.rs
+++ src/controller.rs
@@ -1,5 +1,5 @@
 use core::f32::consts::PI;
-use nalgebra::{Vector2, Vector3, norm};
+use nalgebra::{Vector2, Vector3, norm, clamp};
 
 /// Nonlinear drone controller
 struct Controller {
@@ -11,9 +11,42 @@ struct Controller {
     max_torque: f32,
     /// Vehicle's moment of inertia about three axes
     MOI: Vector3,
+    /// P-coefficient for altitude controller
+    KpAlt: f32,
+    /// Maximal descent rate
+    MaxDescentRate: f32,
+    /// Maximal ascent rate
+    MaxAscentRate: f32,
+    /// P-coefficient for vertical acceleration controller
+    KpAltAcc: f32,
+    /// Drone mass
+    Mass: f32,
+    /// Maximal thrust
+    MaxThrust: f32,
 }
 
 impl Controller {
+    /// Altitude controller
+    /// Return thrust to set given current state
+    /// attitude in form roll/pitch/yaw
+    pub fn altitude(
+        self,
+        target_alt: f32,
+        target_vetical_velocity: f32,
+        altitude: f32,
+        vertical_velocity: f32,
+        attitude: Vector3,
+        feed_forward_acceleration: f32) -> f32 {
+
+        let mut v_cmd = self.KpAlt * (target_alt - altitude);
+        v_cmd += target_vertical_velocity;
+        v_cmd = clamp(dot_cmd, -self.MaxDescentRate, self.MaxAscentRate);
+        let acc_cmd = feed_forward_acceleration;
+        acc_cmd += self.KpAltAcc * (v_cmd - vertical_velocity);
+        let thrust = self.Mass * acc_cmd / (cos(attitude[0]) * cos(attitude[1]);
+        clamp(thrust, 0.0, self.MaxThrust)
+    }
+
     /// Roll-pitch controller.
     /// cmd: target acceleration (north, east)
     /// attitude: roll, pitch, yaw
@@ -24,7 +57,7 @@ impl Controller {
 
     /// Body-rate controller.
     /// Gets: target body rates and actual ones
-    /// Returns: roll, pitch and yaw moments in N*m
+    /// Returns: roll, pitch and yaw moments in N * m
     pub fn body_rate(self, target: Vector3, current: Vector3) -> Vector3 {
         let err = target - current;
         let mut cmd = self.MOI * (self.KpPQR * err);
